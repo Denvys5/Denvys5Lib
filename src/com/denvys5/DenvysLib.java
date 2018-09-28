6105acd59f039e17b69d37c1d2087f4d6ed816c9
Denvys5 <denvys5@gmail.com>
2018-09-28 22:33:03 +0300

    Initial commit

--- src/com/denvys5/DenvysLib.java
+++ src/com/denvys5/DenvysLib.java
@@ -5,10 +5,10 @@ public class DenvysLib {
     public static final String siteDir = "minenew";
     public static final String[] p = {"wireshark", "cheat"};
     public static final String http = "http://";
-    public static final String masterVersion = "1.8";
     public static String baseconf;
     public static String configName;
     public static boolean debug = true;
+    public static final String masterVersion = "1.0";
 
     public static void initConfig(String path, String filename) {
         if (path == null || path.isEmpty())
