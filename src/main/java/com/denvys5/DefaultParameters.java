c14f5c57e6d3f827bfe650c53cc38475c4a529d1
Denis <denvys5@gmail.com>
2020-05-18 14:40:35 +0300

    Overhauling lib in progress

--- src/main/java/com/denvys5/DefaultParameters.java
+++ src/main/java/com/denvys5/DefaultParameters.java
@@ -3,8 +3,4 @@ package com.denvys5;
 public interface DefaultParameters {
     boolean DEFAULT_DEBUG = false;
     String DEBUG_PROPERTY = "debug";
-    String domain = "sagitarium.org";
-    String siteDir = "minenew";
-    String[] p = {"wireshark", "cheat"};
-    String http = "http://";
 }
