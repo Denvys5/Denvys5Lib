0d57cde1a120cc03345bfd35e341fe39fc7f865e
Denis <denvys5@gmail.com>
2020-08-06 15:17:40 +0300

    Updated LICENSE

--- src/main/java/com/denvys5/Config.java
+++ src/main/java/com/denvys5/Config.java
@@ -1,6 +1,23 @@
-package com.denvys5;
 
-import java.io.File;
+
+/*
+ *  Copyright (C) 2020  Denys Vysoven
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.denvys5;
 
 public class Config implements DefaultParameters{
     private boolean debug;
